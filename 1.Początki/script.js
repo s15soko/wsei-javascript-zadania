61234da81f63edc80d1f253b5ed13259c35f5dfd
s15soko <arkadiusz.chmenia@gmail.com>
2020-12-26 15:00:05 +0100

    #1 beginnings

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+// #1
+
+console.log("Arek");
+
+// #2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// #3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World");
