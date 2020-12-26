325a442a1c1006c63fe3b8c2f34d08732296f347
s15soko <arkadiusz.chmenia@gmail.com>
2020-12-26 15:09:21 +0100

    #2 variables and constants

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,34 @@
+// #1
+
+class MyClass
+{
+
+}
+
+var number = 1;
+var text = "Hello";
+var someMath = 1 + "dwa";
+var bool = true;
+var special = new MyClass();
+
+console.log(number);
+console.log(text);
+console.log(someMath);
+console.log(bool);
+console.log(special);
+
+// #2
+
+var num1 = 2;
+var num2 = 3;
+var sum = 0;
+
+sum += num1;
+sum += num2;
+
+console.log(sum);
+
+// #3
+
+var myVariable;
+console.log(myVariable); // undefined
